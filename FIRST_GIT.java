1ef3f67068f05ce1455b574248d762df5f668a8d
Fernando García-Mascaraque <fergms1988@gmail.com>
2021-10-19 20:55:51 +0200

    más palabras

--- FIRST_GIT.java
+++ FIRST_GIT.java
@@ -4,7 +4,8 @@ public class FIRST_GIT {
 	public static void main(String args[])
 	{
 	
-	System.out.println("Git, tienes que funcionar bien");
+	System.out.println("Git, tienes que funcionar bien."
+			+ "Por fin");
 
 }
 }
